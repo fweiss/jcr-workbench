ec96a6e278aa64c03bc27fabdad38644d053c2b5
frankw <frankw@f42e7a9f-ca6c-0410-b27d-4f38f509c999>
2010-10-30 13:06:06 +0000

    Refactor: use Custom tree to displayt node type hierarchy.
    git-svn-id: file:///home/svn/uttama@63 f42e7a9f-ca6c-0410-b27d-4f38f509c999

--- jcr-workbench/src/com/uttama/jcr/workbench/view/NodeTypeHierarchyPanel.java
+++ jcr-workbench/src/com/uttama/jcr/workbench/view/NodeTypeHierarchyPanel.java
@@ -4,11 +4,14 @@ import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
 import javax.swing.JPanel;
 import javax.swing.JTree;
+import javax.swing.tree.TreeSelectionModel;
 
 import com.uttama.jcr.workbench.events.ModelChangeEvent;
 import com.uttama.jcr.workbench.events.ModelChangeListener;
 import com.uttama.jcr.workbench.model.NodeModel;
 import com.uttama.jcr.workbench.model.NodeTypeModel;
+import com.uttama.jcr.workbench.view.swing.CustomJTree;
+import com.uttama.jcr.workbench.view.swing.JCRTreeCellRenderer;
 
 public class NodeTypeHierarchyPanel
 extends JPanel
@@ -24,7 +27,16 @@ implements ModelChangeListener {
 		this.nodeTypeModel = nodeTypeModel;
 	}
 	private void createViews() {
-		typeTree = new JTree();
+		//typeTree = new JTree();
+		
+		typeTree = new CustomJTree(null);
+		typeTree.setShowsRootHandles(true);
+		typeTree.putClientProperty("JTree.lineStyle", "Angled");
+		typeTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+		typeTree.setRootVisible(true);
+		typeTree.setCellRenderer(new JCRTreeCellRenderer());
+
+        
 		add(typeTree);
 	}
 	@Override
